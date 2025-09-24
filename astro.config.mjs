11cf19bb8a7bee0a29ce0818725a8a772e227905
Aristocratic Arietta <aristocraticarietta@proton.me>
2025-09-24 00:54:50 -0700

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 import 'dotenv/config';
 import alpinejs from '@astrojs/alpinejs';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        build: {
-            target: 'esnext',
-        },
-        plugins: [tailwindcss(), alpinejs()],
-    },
-    output: 'server',
+  vite: {
+      build: {
+          target: 'esnext',
+      },
+      plugins: [tailwindcss(), alpinejs()],
+  },
+
+  output: 'server',
+  adapter: netlify(),
 });
